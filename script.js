9167e3d61776f32fbfa0e59629c1bd977dadf78e
Nikita Malichenko <nikitam@123software.ru>
2018-05-26 17:35:16 +0300

    2

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
-// создать подключение
 var socket = new WebSocket("ws://localhost:8081");
+var doc = document;
+var txtSend = doc.getElementById('txtSend');
+var btnSend = doc.getElementById('btnSend');
+var txtLogin = doc.getElementById('txtLogin');
+var btnLogin = doc.getElementById('btnLogin');
 
-// отправить сообщение из формы publish
-document.forms.publish.onsubmit = function() {
-    var outgoingMessage = this.message.value;
 
-    socket.send(outgoingMessage);
-    return false;
+
+socket.onmessage = function (event) {
+  var incomingMessage = event.data;
+  var envelop = JSON.parse(incomingMessage);
+  dispatcher(envelop);
 };
 
-// обработчик входящих сообщений
-socket.onmessage = function(event) {
-    var incomingMessage = event.data;
-    showMessage(incomingMessage);
-};
+var Envelop = function (modul, command, msg) {
+  this.modul = modul;
+  this.command = command;
+  this.msg = msg;
+}
 
-// показать сообщение в div#subscribe
-function showMessage(message) {
-    var messageElem = document.createElement('div');
-    messageElem.appendChild(document.createTextNode(message));
-    document.getElementById('subscribe').appendChild(messageElem);
+function dispatcher(envelop) {
+  switch (envelop.modul) {
+    case 'profile':
+      var profile = new Profile(envelop);
+      profile.showUsers();
+      break;
+    case 'ra':
+
+      break;
+    case 'chat':
+      var chat = new Chat(envelop.msg);
+      chat.showMsg();
+      break;
+  }
+}
+
+var Chat = function (inMsg) {
+  this.inMsg = inMsg;
+}
+
+Chat.prototype.showMsg = function () {
+  var messageElem = document.createElement('div');
+  messageElem.appendChild(document.createTextNode(this.inMsg));
+  document.getElementById('subscribe').appendChild(messageElem);
+}
+
+btnSend.onclick = function () {
+  var env = new Envelop('chat', 'msg', txtLogin.value + ': ' + txtSend.value)
+  socket.send(JSON.stringify(env));
+}
+
+btnLogin.onclick = function () {
+  var env = new Envelop('ra', 'msg', txtLogin.value)
+  socket.send(JSON.stringify(env));
+}
+
+var Profile = function (arr) {
+  this.arr = arr;
+}
+
+Profile.prototype.showUsers = function () {
+  var usersElem = document.createElement('div');
+  usersElem.appendChild(document.createTextNode(this.arr.msg));
+  document.getElementById('userList').appendChild(usersElem);
 }
--- server.js
+++ server.js
@@ -1,29 +1,88 @@
 var WebSocketServer = new require('ws');
 
-// подключенные клиенты
-var clients = {};
+var clients = [];
 
-// WebSocket-сервер на порту 8081
 var webSocketServer = new WebSocketServer.Server({
-    port: 8081
+  port: 8081
 });
-webSocketServer.on('connection', function(ws) {
 
-    var id = Math.random();
-    clients[id] = ws;
-    console.log("новое соединение " + id);
+webSocketServer.on('connection', function (ws) {
+  var id = Math.random();
+  var user = new User(id, ws, '');
+  clients.push(user);
 
-    ws.on('message', function(message) {
-        console.log('получено сообщение ' + message);
+  ws.on('message', function (message) {
+    console.log('получено сообщение ' + message);
+    var envelop = JSON.parse(message);
+    dispatcher(envelop);
+  });
 
-        for (var key in clients) {
-            clients[key].send(message);
-        }
-    });
-
-    ws.on('close', function() {
-        console.log('соединение закрыто ' + id);
-        delete clients[id];
-    });
+  ws.on('close', function () {
+    console.log('соединение закрыто ' + id);
+    delete clients.id;
+  });
 
 });
+
+var Envelop = function (modul, command, msg) {
+  this.modul = modul;
+  this.command = command;
+  this.msg = msg;
+}
+
+var User = function (id, ws, userName) {
+  this.id = id;
+  this.ws = ws;
+  this.userName = userName;
+}
+
+function dispatcher(envelop) {
+  switch (envelop.modul) {
+    case 'profile':
+
+      break;
+    case 'ra':
+      var ra = new RA(envelop);
+      ra.handleruser();
+      profile();
+      break;
+    case 'chat':
+      var chat = new Chat(envelop);
+      chat.sendMSG();
+      break;
+  }
+}
+
+var Chat = function (envelop) {
+  this.envelop = envelop;
+}
+
+Chat.prototype.sendMSG = function () {
+  for (let i = 0; i < clients.length; i++) {
+    clients[i].ws.send(JSON.stringify(this.envelop));
+  }
+}
+
+var RA = function (envelop) {
+  this.envelop = envelop;
+}
+
+RA.prototype.handleruser = function () {
+  for (var i = 0; i < clients.length; i++) {
+    if (clients[i].userName === '') {
+      clients[i].userName = this.envelop.msg;
+      break;
+    }
+  }
+}
+
+function profile() {
+  var str = "";
+  for (var i = 0; i < clients.length; i++) {
+    str += clients[i].userName + ",";
+  }
+  var evp = new Envelop('profile', 'msg', str)
+  for (let i = 0; i < clients.length; i++) { 
+    clients[i].ws.send(JSON.stringify(evp));
+  }
+}
